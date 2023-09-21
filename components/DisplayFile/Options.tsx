54660c9238e2fe283af71586917bbcc66af3e1f6
root <root@Sanusi.hstgr.cloud>
2023-09-21 12:24:00 +0000

    cleaned Options.tsx

--- components/DisplayFile/Options.tsx
+++ components/DisplayFile/Options.tsx
@@ -1,62 +1,15 @@
 import { useRouter } from "next/router";
-
-import PDFToJPG from "./Options/PDFtoJPG";
-import { CompressPDF } from "./Options/CompressPDF";
 import type { edit_page as _ } from "../../content";
-import { SplitPDF } from "./Options/SplitPDF";
 
 export interface OptionsProps {
   layout?: string;
   edit_page: _;
 }
 
-// implemented
-const MergePdf = ({ merge_pdf }: { merge_pdf: string }) => (
-  <div className="alert alert-info">
-    <bdi>{merge_pdf}</bdi>
-  </div>
-);
-// const JpgToPDF = () => {
-//   // needs implementation
-// };
-// const HTMLToPDF = () => {
-//   // NEEDS implementation
-// };
 
-// const PDFToPDFA = () => {
-//   // needs implementation
-// };
-// // NEEDS implementation only on premium
-// const PDFToWORD = () => {};
-// // NEEDS implementation only on premium
-// const PDFToEXCEL = () => {};
 
 const Options = ({ layout, edit_page }: OptionsProps) => {
-  let componentToRender: JSX.Element | null;
-
-  const router = useRouter();
-  let c = router.asPath.replace(/^\/[a-z]{2}\//, "").replace(/^\//, "");
-  switch (layout) {
-    case "compress-pdf":
-      componentToRender = (
-        <CompressPDF c={c} options={edit_page["compress_pdf"]} />
-      );
-      break;
-    case "split-pdf":
-      // Implementation of SplitPDF logic goes here
-      componentToRender = <SplitPDF />
-      break;
-    case "merge-pdf":
-      componentToRender = <MergePdf merge_pdf={edit_page["merge_pdf"]} />;
-      break;
-    case "jpg-to-pdf":
-      componentToRender = <PDFToJPG c={c} />;
-      break;
-    default:
-      componentToRender = null;
-  }
-
-  return componentToRender;
+  return <></>
 };
 
 export default Options;
