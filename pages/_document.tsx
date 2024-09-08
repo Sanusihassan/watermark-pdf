4a9abe15d20a19e6b46656186ccab9a78a5fee07
Sanusi Hassan <sanusihassan122@gmail.com>
2024-09-08 20:22:11 +0200

    added ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,10 @@ const MyDocument = ({ lang }: { lang: string }) => {
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         />
         <meta name="google-adsense-account" content="ca-pub-7391414384206267" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7391414384206267"
+          cross-origin="anonymous"></script>
       </Head>
       <body>
         <Main />
