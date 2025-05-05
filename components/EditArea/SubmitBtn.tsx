c6800cc10749dc7213d5bf1b852e4385e048ceb2
Sanusi Hassan <sanusihassan122@gmail.com>
2025-05-05 11:38:00 +0200

    removed pricing redirect

--- components/EditArea/SubmitBtn.tsx
+++ components/EditArea/SubmitBtn.tsx
@@ -25,20 +25,11 @@ export function SubmitBtn({
   return (
     <button
       className={`submit-btn btn btn-lg text-white position-relative overflow-hidden ${k} grid-footer`}
-      onClick={async () => {
+      onClick={() => {
         dispatch(setField({ isSubmitted: true }));
         dispatch(setField({ showOptions: false }));
-        const status = await fetchSubscriptionStatus();
-        if (!status && !canUseSiteToday(10)) {
-          if (!canUseSiteToday(1)) {
-            if (typeof window !== "undefined") {
-              window.open(`${(lang === "" ? "" : "/") + lang}/pricing`, "_blank");
-            }
-          } else {
-            if (submitBtn) {
-              submitBtn?.current?.click();
-            }
-          }
+        if (submitBtn) {
+          submitBtn?.current?.click();
         }
       }}
     // disabled={errorMessage.length > 0}
